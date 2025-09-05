52341d35235567aa23d34513ecbc87652deb0f8d
CuteCoder <yamamotogenji7@gmail.com>
2025-09-05 18:45:32 +0900

    Update projects.ts

--- src/projects.ts
+++ src/projects.ts
@@ -6,6 +6,24 @@ export const projects = [
       'A URL shortening service with a built-in analytics dashboard. It tracks link clicks, geographic data, and user behavior in real-time. Currently has over 7000 users and more than a million link clicks.',
   },
   {
+    name: 'zihni',
+    link: '',
+    description:
+      'AI-powered analyst agents for VCs. Built autonomous LLM-based agents to automate early-stage tasks like pitch deck analysis, market research, and investment memo generation—drastically reducing due diligence time.',
+  },
+  {
+    name: 'bazaar',
+    link: '',
+    description:
+      'A full-stack self-healing web scraper that leverages AI to automate complex web scraping workflows—over 10 million records collected, with a 90% reduction in manual data collection.',
+  },
+  {
+    name: 'docker reverse proxy',
+    link: 'https://github.com/AmoabaKelvin/docker-reverse-proxy',
+    description:
+      'A custom Docker reverse proxy built from scratch to understand how tools like Traefik and NGINX work under the hood. Focused on dynamic service discovery and routing.',
+  },
+{
     name: 'is-temp.com',
     link: 'https://is-temp.com',
     description:
@@ -28,24 +46,6 @@ export const projects = [
     link: '',
     description:
       'A beautiful, clean Docker logs viewer. Easily browse system logs, container output, and more—all in one unified interface.',
-  },
-  {
-    name: 'zihni',
-    link: '',
-    description:
-      'AI-powered analyst agents for VCs. Built autonomous LLM-based agents to automate early-stage tasks like pitch deck analysis, market research, and investment memo generation—drastically reducing due diligence time.',
-  },
-  {
-    name: 'bazaar',
-    link: '',
-    description:
-      'A full-stack self-healing web scraper that leverages AI to automate complex web scraping workflows—over 10 million records collected, with a 90% reduction in manual data collection.',
-  },
-  {
-    name: 'docker reverse proxy',
-    link: 'https://github.com/AmoabaKelvin/docker-reverse-proxy',
-    description:
-      'A custom Docker reverse proxy built from scratch to understand how tools like Traefik and NGINX work under the hood. Focused on dynamic service discovery and routing.',
   },
   {
     name: 'meta-scout',
