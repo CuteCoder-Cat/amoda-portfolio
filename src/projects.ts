a37f9376144feb3d67a71e09160cea1e00f6ddc1
CuteCoder <yamamotogenji7@gmail.com>
2025-09-05 18:54:29 +0900

    Update projects.ts

--- src/projects.ts
+++ src/projects.ts
@@ -59,33 +59,4 @@ export const projects = [
     description:
       'Active contributor to OpenStatus. 🏓 The open-source monitoring platform with incident management 🏓',
   },
-  // {
-  //   name: 'Scrapy BigQuery Pipeline',
-  //   link: 'https://github.com/8W9aG/scrapy-bigquery',
-  //   description:
-  //     'Contributed to the development of an open-source bigquery pipeline for Scrapy framework',
-  // },
-  // {
-  //   name: 'Vinyl',
-  //   link: 'https://github.com/AmoabaKelvin/vinyl-backend',
-  //   description: `A music store built with Django Rest Framework, PostgreSQL and integrated with Stripe
-  //   This project is a backend API for a music store, with features like authentication, authorization, payment processing.
-  //   It allows users to buy music and artists to sell their music by uploading it to the platform and getting paid for every sale.
-  //   `,
-  // },
-  // {
-  //   name: 'Studyfuse',
-  //   link: '',
-  //   description:
-  //     'Studyfuse is an online forum for students and teachers to ask questions and share ideas, developed during my time in college for fellow students.',
-  // },
-  // {
-  //   name: 'Kairos',
-  //   description: `
-  //     A laboratory management system built with Python, Django, PostgreSQL and Bootstrap.
-  //     Kairos solved the problem of record keeping and management of patients' data as well as financial records.
-  //     It allowed to register new parents, capture their data, and store them in a database. Performing revenue analysis and generating reports.
-  //     `,
-  //   link: 'https://github.com/AmoabaKelvin/kairos-medical-center',
-  // },
 ];
